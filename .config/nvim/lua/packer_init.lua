f0a8f3de57368b3566df12b5645748f9ef1f50f2
Viet Vu <vietvudanh@gmail.com>
2022-10-06 14:23:43 +0700

    using lua for nvim

---
+++ .config/nvim/lua/packer_init.lua
@@ -0,0 +1,109 @@
+-----------------------------------------------------------
+-- Plugin manager configuration file
+-----------------------------------------------------------
+
+-- Plugin manager: packer.nvim
+-- url: https://github.com/wbthomason/packer.nvim
+
+-- For information about installed plugins see the README:
+-- neovim-lua/README.md
+-- https://github.com/brainfucksec/neovim-lua#readme
+
+
+-- Automatically install packer
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path
+  })
+  vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
+end
+
+-- Autocommand that reloads neovim whenever you save the packer_init.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost packer_init.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, 'packer')
+if not status_ok then
+  return
+end
+
+-- Install plugins
+return packer.startup(function(use)
+  -- Add you plugins here:
+  use 'wbthomason/packer.nvim' -- packer can manage itself
+
+  -- telescope
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+  -- auto complete
+  use {'ms-jpq/coq_nvim', branch = 'coq'}
+  use {'ms-jpq/coq.artifacts', branch = 'artifacts'}
+
+  -- File explorer
+  use 'kyazdani42/nvim-tree.lua'
+
+  -- Indent line
+  use 'lukas-reineke/indent-blankline.nvim'
+
+  -- Autopair
+  use {
+    'windwp/nvim-autopairs',
+    config = function()
+      require('nvim-autopairs').setup{}
+    end
+  }
+
+  -- Icons
+  use 'kyazdani42/nvim-web-devicons'
+
+  -- Tag viewer
+  use 'preservim/tagbar'
+
+  -- Treesitter interface
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+  }
+
+  -- Color schemes
+  use 'navarasu/onedark.nvim'
+  use 'tanvirtin/monokai.nvim'
+  use { 'rose-pine/neovim', as = 'rose-pine' }
+
+  -- Statusline
+  use {
+    'feline-nvim/feline.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons' },
+  }
+
+  -- git labels
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('gitsigns').setup{}
+    end
+  }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
--- .config/nvim/snippets/all.lua
+++
@@ -1,9 +0,0 @@
-return {
-    s("date", f(function(_, snip)
-        return os.date('%Y-%m-%d')
-    end, {})),
-    s("datetime", f(function(_, snip)
-        return os.date('%Y-%m-%d %H:%M:%S')
-    end, {})),
-    parse("todo", "- [ ] "),
-}
