565e072f7b4f783380c375c184da509c11e7e310
vietvudanh <vietvudanh@gmail.com>
2024-09-16 11:31:17 +0700

    add option arrow left+right

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
--- config.color_scheme = "Batman"
+config.color_scheme = "Batman"
 config.keys = {
 	{
 		key = "d",
@@ -19,6 +19,10 @@ config.keys = {
 		mods = "SUPER|SHIFT",
 		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
 	},
+	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+	-- Make Option-Right equivalent to Alt-f; forward-word
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
 }
 
 -- and finally, return the configuration to wezterm
