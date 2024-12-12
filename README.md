3f888d654eb3dfb344c0963ec3d4377632563150
vietvudanh <vietvudanh@gmail.com>
2024-12-12 14:15:31 +0700

    add vim

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ To sync my config over devices. I have too many at the time writing this.
     1. vim
     2. sublime-text
     3. zsh
+    4. git
+    5. zed
+
+# install n
--- scripts/init.sh
+++ scripts/init.sh
@@ -2,4 +2,11 @@
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -s $CWD/../.tmux.conf ~/.tmux.conf
-ln -s $CWD/../.config/nvim ~/.config/nviM
+ln -s $CWD/../.config/nvim ~/.config/nvim
+mkdir ~/.config/zed \
+    && ln -s $CWD/../.config/zed/setings.json ~/.config/zed/setings.json \
+    && ln -s $CWD/../.config/zed/keymap.json ~/.config/zed/keymap.json
+
+# vim need: https://github.com/dandavison/delta?tab=readme-ov-file
+# brew install git-delta
+ln -s $CWD/../.config/.gitconfig ~/.config/.gitconfig
