c350ecbf34f0f74e1630c5b8391d112eb4dc01ed
vietvudanh <vietvudanh@gmail.com>
2024-12-23 15:42:38 +0700

    add aeorpspace

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,8 +5,8 @@ CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # ln -s $CWD/../.config/nvim ~/.config/nvim
 # mkdir ~/.config/zed \
 #     && ln -s $CWD/../.config/zed/setings.json ~/.config/zed/setings.json \
-#     && ln -s $CWD/../.config/zed/keymap.json ~/.config/zed/keymap.json
-
+#     && ln -s $CWD/../.config/zed/keymap.json ~/.config/zed/keymap.json \
+#     && ln -s $CWD/../.config/zed/themes ~/.config/zed/themes
 # vim need: https://github.com/dandavison/delta?tab=readme-ov-file
 # brew install git-delta
 ln -s $CWD/../.gitconfig ~/.gitconfig
