5e96b13618e6a7cf94b9e75fd7615f94463596e9
vietvudanh <vietvudanh@gmail.com>
2025-04-01 13:28:18 +0700

    add ruff lint imports

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,10 +20,11 @@ set -x PATH $PATH /Users/vietvu/go/bin
 set -x PATH $PATH /Users/vietvu/.cargo/bin
 set -x PATH $PATH /Users/vietvu/.local/bin
 direnv hook fish | source
+mise activate fish | source
 
 ## functions
 function gi
-  curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/$argv
+    curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/$argv
 end
 function initPythonMakefile
     echo -e ".PHONY: init
@@ -47,24 +48,32 @@ requirements: requirements-compile
 
 .PHONY: build
 build:
-\tuv build" > Makefile
+\tuv build
+
+.PHONY: lint
+lint:
+\truff check --select I --fix .
+\truff format" >Makefile
 end
 
 function initPyrightZedCfg
     echo '{
     "venvPath": ".",
     "venv": ".venv"
-}'> ./pyrightconfig.json
+}' >./pyrightconfig.json
 end
 
 function gh-clone
-    cd ~/works/github.com/ ; gh repo clone $argv[1]
+    cd ~/works/github.com/
+    gh repo clone $argv[1]
 end
-
-
 
 ## alias
 alias notes "cd ~/works/github.com/vietvudanh/notes"
 alias dotfiles "cd ~/works/github.com/vietvudanh/dotfiles"
 alias gi-list 'gi list'
 alias gi-py 'gi python,Jetbrains,VisualStudioCode,dotenv,dbt > .gitignore'
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
